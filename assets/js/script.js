cd24db73a15dc0ba093c90d9f1771aa75a38bb2e
restuhaqza <restuhaqza@gmail.com>
2019-02-20 12:02:55 +0700

    add all file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$('.info dd').each(function() {
+    $(this).css({width: $(this).text()+'%'});
+  });
+
+function toMyBlog(){
+      window.location = 'https://www.restuhaqza.com/p/tentang-saya.html'
+}
